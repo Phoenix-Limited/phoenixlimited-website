32f2ddb665e173cea9f7bcc45717ca396ad5b71d
Eli Gibbs <elijah@phoenixlimited.net>
2025-01-03 14:02:29 -0500

    vault backup: 2025-01-03 14:02:28

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/phoenix/Documents/phoenix-limited-resources/Resources/Websites/wiki.phoenixlimited.net/node_modules/infima/dist/css/default/default.css"),
-  require("/home/phoenix/Documents/phoenix-limited-resources/Resources/Websites/wiki.phoenixlimited.net/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/phoenix/Documents/phoenix-limited-resources/Resources/Websites/wiki.phoenixlimited.net/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/phoenix/Documents/phoenix-limited-resources/Resources/Websites/wiki.phoenixlimited.net/src/css/custom.css"),
-];
--- build/assets/js/36994c47.6d3b53d2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwiki_phoenixlimited_net=self.webpackChunkwiki_phoenixlimited_net||[]).push([[858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
