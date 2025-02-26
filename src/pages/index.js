65caf35f1d244a207301e27e9f4e0a6bbd9b8548
Eli Gibbs <elijah@phoenixlimited.net>
2025-02-25 21:07:10 -0500

    button test

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,13 +19,6 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Start Here!
-          </Link>
-        </div>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
             to="mailto:contact@phoenixlimited.net">
             Email Us
           </Link>
