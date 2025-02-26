37a66d6af923e7909e3e9f06fc4ca98fd2873c9d
Eli Gibbs <elijah@phoenixlimited.net>
2025-02-26 16:48:04 -0500

    change footer and add scripts

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from '@theme-original/Footer';
 
-import zammad from './zammad.js';
+import zammad from './zammad';
 
 export default function FooterWrapper(props) {
     return (
