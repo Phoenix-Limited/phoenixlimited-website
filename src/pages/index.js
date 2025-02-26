0d9b66bfd846fe83b081b6021b5cf908df9d1b6d
Eli Gibbs <elijah@phoenixlimited.net>
2025-02-26 18:09:29 -0500

    change footer and feedback

--- src/pages/index.js
+++ src/pages/index.js
@@ -133,7 +133,7 @@ export default function Home() {
         <h2 className={clsx(styles.alignCenter, styles.headerUnderline)}>Want to leave feedback?</h2>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <div className={styles.buttons}>
-          <button id="zammad-feedback-form" className="button button--secondary button--sm">Submit a request?</button>
+          <button id="zammad-feedback-form" className="button button--secondary button--sm">Submit a request!</button>
         </div>
         <br />
         <script id="zammad_form_script" src="https://support.phoenixlimited.net/assets/form/form.js"></script>
