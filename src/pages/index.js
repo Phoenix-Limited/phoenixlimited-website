4feaa207853c1e6d8c3603d979af668d821525bc
Eli Gibbs <elijah@phoenixlimited.net>
2025-02-25 19:27:00 -0500

    button test

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,7 @@ function HomepageHeader() {
             Start Here!
           </Link>
         </div>
+        <br />
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
