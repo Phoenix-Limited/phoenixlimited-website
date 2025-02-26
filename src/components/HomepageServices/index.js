d82cad5da24ad715f59ac631e91a73117ff2ace1
Eli Gibbs <elijah@phoenixlimited.net>
2025-02-26 10:26:49 -0500

    change section 4

--- src/components/HomepageServices/index.js
+++ src/components/HomepageServices/index.js
@@ -35,9 +35,6 @@ const FeatureList = [
 function Service({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
