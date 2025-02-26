a3751a2264829cc9912be5d4f8632a41aa03d9f6
Eli Gibbs <elijah@phoenixlimited.net>
2025-02-26 16:22:41 -0500

    change footer and add scripts

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import Footer from '@theme-original/Footer';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {fas} from '@fortawesome/free-solid-svg-icons'
-
-import zammad from '@site/src/components/zammad.js';
-
 export default function FooterWrapper(props) {
     return (
         <>
             <script src="https://support.phoenixlimited.net/assets/chat/chat-no-jquery.min.js"></script>
             <script src="https://phoenixlimited.net/assets/js/zammad.js"></script>
             <Footer {...props} />
-            <FontAwesomeIcon icon={fas.faCode} size="6x" />
         </>
     );
 }
