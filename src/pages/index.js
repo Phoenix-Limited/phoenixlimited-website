5d76b794cc6117ee04d133eea39a8d306458ce3d
Eli Gibbs <elijah@phoenixlimited.net>
2025-02-26 10:10:30 -0500

    change section 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import HomepageServices from '@site/src/components/HomepageFeatures';
+import HomepageServices from '@site/src/components/HomepageServices';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
