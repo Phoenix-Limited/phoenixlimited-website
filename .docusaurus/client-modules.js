a09fbd0a377ce3da45a14fbcbb8e99c580592855
Eli Gibbs <eligibbs@outlook.com>
2025-08-26 09:23:36 -0400

    github migration

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Volumes/Data/7.10.2025/Documents/wiki-docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Volumes/Data/7.10.2025/Documents/wiki-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Volumes/Data/7.10.2025/Documents/wiki-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Volumes/Data/7.10.2025/Documents/wiki-docs/src/css/custom.css"),
+];
